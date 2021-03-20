cd44c76fba19c964ea76a43071792848acc6a91f
uppnrise <mtbnacar@gmail.com>
2021-03-20 12:20:21 +0300

    create-microservices.bash is created. Multi-module project build script is created.

---
+++ create-microservices.bash
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+mkdir microservices
+cd microservices
+
+spring init \
+--boot-version=2.3.2.RELEASE \
+--build=gradle \
+--java-version=1.8 \
+--packaging=jar \
+--name=product-service \
+--package-name=com.upp.microservices.core.product \
+--groupId=com.upp.microservices.core.product \
+--dependencies=actuator,webflux \
+--version=1.0.0-SNAPSHOT \
+product-service
+
+spring init \
+--boot-version=2.3.2.RELEASE \
+--build=gradle \
+--java-version=1.8 \
+--packaging=jar \
+--name=review-service \
+--package-name=com.upp.microservices.core.review \
+--groupId=com.upp.microservices.core.review \
+--dependencies=actuator,webflux \
+--version=1.0.0-SNAPSHOT \
+review-service
+
+spring init \
+--boot-version=2.3.2.RELEASE \
+--build=gradle \
+--java-version=1.8 \
+--packaging=jar \
+--name=recommendation-service \
+--package-name=com.upp.microservices.core.recommendation \
+--groupId=com.upp.microservices.core.recommendation \
+--dependencies=actuator,webflux \
+--version=1.0.0-SNAPSHOT \
+recommendation-service
+
+spring init \
+--boot-version=2.3.2.RELEASE \
+--build=gradle \
+--java-version=1.8 \
+--packaging=jar \
+--name=product-composite-service \
+--package-name=com.upp.microservices.composite.product \
+--groupId=com.upp.microservices.composite.product \
+--dependencies=actuator,webflux \
+--version=1.0.0-SNAPSHOT \
+product-composite-service
+
+cd ..
