2062987116ce5c2ac3dde9aa555fb5465cc14675
uppnrise <mtbnacar@gmail.com>
2021-04-11 23:25:59 +0300

    product and recommendation mongodb docs are created. reviews entity created. repositories are defined.

---
+++ microservices/review-service/src/main/java/com/upp/microservices/core/review/persistence/ReviewRepository.java
@@ -0,0 +1,13 @@
+package com.upp.microservices.core.review.persistence;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface ReviewRepository extends CrudRepository<ReviewEntity, String> {
+
+    @Transactional(readOnly = true)
+    List<ReviewEntity> findByProductId(int productId);
+
+}
