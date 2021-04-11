282e30c4fd81bbb5a8969d54e40c6f43fd5ebf64
uppnrise <mtbnacar@gmail.com>
2021-04-11 13:13:54 +0300

    OAS3(Open Api Specification 3) is added to product-composite-service. An basic documentation is defined.

---
+++ microservices/product-composite-service/src/main/java/com/upp/microservices/composite/product/config/SwaggerConfig.java
@@ -0,0 +1,63 @@
+package com.upp.microservices.composite.product.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Value("${api.common.version}")
+    String apiVersion;
+    @Value("${api.common.title}")
+    String apiTitle;
+    @Value("${api.common.description}")
+    String apiDescription;
+    @Value("${api.common.termsOfServiceUrl}")
+    String apiTermsOfServiceUrl;
+    @Value("${api.common.license}")
+    String apiLicense;
+    @Value("${api.common.licenseUrl}")
+    String apiLicenseUrl;
+    @Value("${api.common.contact.name}")
+    String apiContactName;
+    @Value("${api.common.contact.url}")
+    String apiContactUrl;
+    @Value("${api.common.contact.email}")
+    String apiContactEmail;
+
+    /**
+     * Will exposed on $HOST:$PORT/swagger-ui/index.html
+     *
+     * @return docket object - swagger documentation.
+     */
+    @Bean
+    public Docket apiDocumentation() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.upp.microservices.composite.product"))
+                .paths(PathSelectors.any())
+                .build()
+                .globalResponses(HttpMethod.GET, Collections.emptyList())
+                .apiInfo(new ApiInfo(
+                        apiTitle,
+                        apiDescription,
+                        apiVersion,
+                        apiTermsOfServiceUrl,
+                        new Contact(apiContactName, apiContactUrl, apiContactEmail),
+                        apiLicense,
+                        apiLicenseUrl,
+                        Collections.emptyList()
+                ));
+    }
+
+}
